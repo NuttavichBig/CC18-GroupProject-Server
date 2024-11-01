cb8b01d82798deba40d6c4af61d48d2b88da59c1
NuttavichBig <nuttavich.si@gmail.com>
2024-11-01 10:47:25 +0700

    Complete hotel path validate

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -79,8 +79,6 @@ const registerSchema = Joi.object({
             'date.max': 'Birth date must be a date in the past.'
         })
 })
-
-
 const loginSchema = Joi.object({
     email: Joi
         .string()
@@ -100,7 +98,6 @@ const loginSchema = Joi.object({
             "string.pattern.base": "Email or Password incorrect."
         }),
 })
-
 const updateUserSchema = Joi.object({
     firstName: Joi
         .string()
@@ -149,8 +146,6 @@ const updateUserSchema = Joi.object({
             'date.max': 'Birth date must be a date in the past.'
         })
 })
-
-
 const forgetPasswordSchema = Joi.object({
     password: Joi
         .string()
@@ -171,9 +166,271 @@ const forgetPasswordSchema = Joi.object({
             'any.only': "password does not match.",
             "string.empty": "Confirm password is required.",
         }),
+});
+
+// Hotel path
+const getHotelQuerySchema = Joi.object({
+    search: Joi
+        .string()
+        .optional()
+        .allow('')
+        .messages({
+            'string.base': 'Search term must be a string'
+        }),
+    maxPrice: Joi
+        .number()
+        .optional()
+        .greater(Joi.ref('minPrice'))
+        .messages({
+            'number.base': 'Max price must be a number',
+            'number.greater': '"maxPrice" must be greater than "minPrice"',
+        }),
+    minPrice: Joi
+        .number()
+        .optional()
+        .messages({
+            'number.base': 'Min price must be a number'
+        }),
+    star: Joi
+        .number()
+        .integer()
+        .min(1)
+        .max(5)
+        .optional()
+        .messages({
+            'number.base': 'Star rating must be a number',
+            'number.integer': 'Star rating must be an integer',
+            'number.min': 'Star rating must be at least 1',
+            'number.max': 'Star rating must be at most 5'
+        }),
+    orderBy: Joi
+        .string()
+        .valid('asc', 'desc')
+        .default('asc')
+        .optional()
+        .messages({
+            'any.only': 'Order by must be either "asc" or "desc"',
+            'string.base': 'Order by must be a string'
+        }),
+    sortBy: Joi
+        .string()
+        .valid('rating', 'price', 'id')
+        .default('id')
+        .optional()
+        .messages({
+            'any.only': 'Sort by must be one of "rating", "price", or "id"',
+            'string.base': 'Sort by must be a string'
+        }),
+    facilities: Joi
+        .array()
+        .items(Joi.string())
+        .optional()
+        .messages({
+            'array.base': 'Facilities must be an array',
+            'array.includes': 'Facilities must contain only strings'
+        }),
+    limit: Joi
+        .number()
+        .integer()
+        .min(1)
+        .default(10)
+        .messages({
+            'number.base': 'Limit must be a number',
+            'number.integer': 'Limit must be an integer',
+            'number.min': 'Limit must be greater than or equal to 1',
+        }),
+    page: Joi
+        .number()
+        .integer()
+        .min(1)
+        .default(1)
+        .messages({
+            'number.base': 'Page must be a number',
+            'number.integer': 'Page must be an integer',
+            'number.min': 'Page must be greater than or equal to 1',
+        }),
 })
+// .and('maxPrice', 'minPrice'); // Sure that ,maxPrice amd minPrice are provided together
 
+const createHotelSchema = Joi.object({
+    name: Joi
+        .string()
+        .required()
+        .messages({
+            'string.base': 'Name must be a string',
+            'any.required': 'Name is required'
+        }),
+    detail: Joi
+        .string()
+        .optional()
+        .allow('')
+        .messages({
+            'string.base': 'Detail must be a string'
+        }),
+    address: Joi
+        .string()
+        .required()
+        .messages({
+            'string.base': 'Address must be a string',
+            'any.required': 'Address is required'
+        }),
 
+    lat: Joi
+        .number()
+        .min(-90)
+        .max(90)
+        .required()
+        .messages({
+            'number.base': 'Latitude must be a number',
+            'number.min': 'Latitude must be between -90 and 90',
+            'number.max': 'Latitude must be between -90 and 90',
+            'any.required': 'Latitude is required'
+        }),
+
+    lng: Joi
+        .number()
+        .min(-180)
+        .max(180)
+        .required()
+        .messages({
+            'number.base': 'Longitude must be a number',
+            'number.min': 'Longitude must be between -180 and 180',
+            'number.max': 'Longitude must be between -180 and 180',
+            'any.required': 'Longitude is required'
+        }),
+
+    star: Joi
+        .number()
+        .integer()
+        .min(1)
+        .max(5)
+        .optional()
+        .messages({
+            'number.base': 'Star rating must be a number',
+            'number.integer': 'Star rating must be an integer',
+            'number.min': 'Star rating must be at least 1',
+            'number.max': 'Star rating must be at most 5',
+            'any.required': 'Star rating is required'
+        }),
+
+    checkInTime: Joi
+        .string()
+        .pattern(/^([01]\d|2[0-3]):([0-5]\d)$/)
+        .required()
+        .messages({
+            'string.base': 'Check-in time must be a string',
+            'string.pattern.base': 'Check-in time must be in "HH:MM" format',
+            'any.required': 'Check-in time is required'
+        }),
+
+    checkOutTime: Joi.string()
+        .pattern(/^([01]\d|2[0-3]):([0-5]\d)$/)
+        .required()
+        .messages({
+            'string.base': 'Check-out time must be a string',
+            'string.pattern.base': 'Check-out time must be in "HH:MM" format',
+            'any.required': 'Check-out time is required'
+        }),
+
+    facilityHotel: Joi
+        .object()
+        .pattern(Joi.boolean())
+        .required()
+        .messages({
+            'object.base': 'Facilities must be an object with boolean values',
+            'any.required': 'Facilities are required'
+        })
+});
+
+const updateHotelSchema = Joi.object({
+    name: Joi
+        .string()
+        .required()
+        .messages({
+            'string.base': 'Name must be a string',
+            'any.required': 'Name is required'
+        }),
+    detail: Joi
+        .string()
+        .optional()
+        .allow('')
+        .messages({
+            'string.base': 'Detail must be a string'
+        }),
+    address: Joi
+        .string()
+        .required()
+        .messages({
+            'string.base': 'Address must be a string',
+            'any.required': 'Address is required'
+        }),
+
+    lat: Joi
+        .number()
+        .min(-90)
+        .max(90)
+        .required()
+        .messages({
+            'number.base': 'Latitude must be a number',
+            'number.min': 'Latitude must be between -90 and 90',
+            'number.max': 'Latitude must be between -90 and 90',
+            'any.required': 'Latitude is required'
+        }),
+
+    lng: Joi
+        .number()
+        .min(-180)
+        .max(180)
+        .required()
+        .messages({
+            'number.base': 'Longitude must be a number',
+            'number.min': 'Longitude must be between -180 and 180',
+            'number.max': 'Longitude must be between -180 and 180',
+            'any.required': 'Longitude is required'
+        }),
+
+    star: Joi
+        .number()
+        .integer()
+        .min(1)
+        .max(5)
+        .optional()
+        .messages({
+            'number.base': 'Star rating must be a number',
+            'number.integer': 'Star rating must be an integer',
+            'number.min': 'Star rating must be at least 1',
+            'number.max': 'Star rating must be at most 5',
+            'any.required': 'Star rating is required'
+        }),
+
+    checkInTime: Joi
+        .string()
+        .pattern(/^([01]\d|2[0-3]):([0-5]\d)$/)
+        .required()
+        .messages({
+            'string.base': 'Check-in time must be a string',
+            'string.pattern.base': 'Check-in time must be in "HH:MM" format',
+            'any.required': 'Check-in time is required'
+        }),
+
+    checkOutTime: Joi.string()
+        .pattern(/^([01]\d|2[0-3]):([0-5]\d)$/)
+        .required()
+        .messages({
+            'string.base': 'Check-out time must be a string',
+            'string.pattern.base': 'Check-out time must be in "HH:MM" format',
+            'any.required': 'Check-out time is required'
+        }),
+
+    facilityHotel: Joi
+        .object()
+        .pattern(Joi.boolean())
+        .required()
+        .messages({
+            'object.base': 'Facilities must be an object with boolean values',
+            'any.required': 'Facilities are required'
+        })
+});
 
 
 
@@ -198,7 +455,14 @@ const validateQuery = (schema) => (req, res, next) => {
 
 
 // Export
+
+// auth
 module.exports.registerValidator = validateSchema(registerSchema)
 module.exports.loginValidator = validateSchema(loginSchema)
 module.exports.updateUserValidator = validateSchema(updateUserSchema)
 module.exports.forgetPasswordValidator = validateSchema(forgetPasswordSchema)
+
+// hotel
+module.exports.getHotelQueryValidator = validateQuery(getHotelQuerySchema)
+module.exports.createHotelValidator = validateSchema(createHotelSchema)
+module.exports.updateHotelValidator = validateSchema(updateHotelSchema)
